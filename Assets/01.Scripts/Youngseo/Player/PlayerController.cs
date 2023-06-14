251b253d3930c53b4ac33fcfd51dd473651d57a9
leeyoungseo6 <dudsj9782@gmail.com>
2023-06-14 16:14:48 +0900

    플레이어 이동 하는 중

--- Assets/01.Scripts/Youngseo/Player/PlayerController.cs
+++ Assets/01.Scripts/Youngseo/Player/PlayerController.cs
@@ -10,12 +10,16 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField]
     private LayerMask Star;
-    private Transform _starTrm;
+    private Collider2D[] _starCollsA;
+    private Collider2D[] _starCollsB;
+    private Vector3 _targetStar;
     [SerializeField]
     private Transform _ropeTrm;
     private Vector3 _startVec;
+    private float _toStarDis;
+    private bool _left;
 
-    private float _orbitDir;
+    private float _orbitDir = 0;
 
     private bool _startOrbit = false;
 
@@ -40,8 +44,12 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         PlayerMove();
+        SetScoreText();
+    }
+
+    private void LateUpdate()
+    {
         OrbitStar();
-        ScoreText();
     }
 
     private void PlayerMove()
@@ -51,58 +59,84 @@ public class PlayerController : MonoBehaviour
 
     private void OrbitStar()
     {
-        
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            _starTrm = Physics2D.OverlapCircle(transform.position, 6f, Star).transform;
-            if (_starTrm == null) return;
-            _starTrm.position += new Vector3(0, 0, 0.1f);
+            Targeting();
             _startVec = transform.position;
         }
-        if (Input.GetKey(KeyCode.Space) && _starTrm != null)
-        {
-            IsRightTri();
-            if (_startOrbit)
-            {
-                float z = Mathf.Atan2(_starTrm.position.y - transform.position.y, _starTrm.position.x - transform.position.x) * Mathf.Rad2Deg;
-                transform.rotation = Quaternion.Euler(0, 0, z + _orbitDir);
-            }
-        }
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            _walls[0].color = new Color(0.8f, 0.4f, 0.4f);
-            _walls[1].color = new Color(0.8f, 0.4f, 0.4f);
-            
-            _ropeTrm.localScale = new Vector3(0, 0, 0);
-            _startOrbit = false;
-            _starTrm = null;
-        }
-    }
 
-    private void IsRightTri()
-    {
-        float a = Vector3.Distance(_startVec, transform.position);
-        float b = Vector3.Distance(_starTrm.position, transform.position);
-        float c = Vector3.Distance(_starTrm.position, _startVec);
-        float p = Mathf.Pow(a, 2) + Mathf.Pow(b, 2) - Mathf.Pow(c, 2);
-        SetLope(b);
-        if (p > 0 && !_startOrbit)
+        if (Input.GetKey(KeyCode.Space))
         {
             _walls[0].color = new Color(0.6f, 0.8f, 0.8f);
             _walls[1].color = new Color(0.6f, 0.8f, 0.8f);
 
-            _startOrbit = true;
-            int x = _starTrm.position.x < transform.position.x ? 1 : -1;
-            int y = transform.up.normalized.y > 0 ? 1 : -1;
-            _orbitDir = x * y == 1 ? 181: -1;
-            _orbitDir += Time.deltaTime * _speed * x * y;
+            if (IsRightTri())
+            {
+                float angle = Mathf.Atan2(_targetStar.y - transform.position.y, _targetStar.x - transform.position.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.Euler(0, 0, angle + _orbitDir); 
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            _walls[0].color = new Color(0.8f, 0.4f, 0.4f);
+            _walls[1].color = new Color(0.8f, 0.4f, 0.4f);
+
+            _ropeTrm.localScale = Vector3.zero;
+            _startOrbit = false;
+            _left = false;
         }
     }
 
-    private void SetLope(float length)
+    private void Targeting()
     {
-        float z = Mathf.Atan2(_starTrm.position.y - _ropeTrm.position.y, _starTrm.position.x - _ropeTrm.position.x) * Mathf.Rad2Deg;
-        _ropeTrm.rotation = Quaternion.Euler(0, 0, z);
+        _toStarDis = 100;
+        _starCollsA = Physics2D.OverlapBoxAll(new Vector2(-2f, transform.position.y), new Vector2(2f, 18f), transform.rotation.z, Star);
+        _starCollsB = Physics2D.OverlapBoxAll(new Vector2(2f, transform.position.y), new Vector2(2f, 18f), transform.rotation.z, Star);
+
+        foreach (Collider2D target in _starCollsA)
+        {
+            float distance = Vector3.Distance(target.transform.position, transform.position);
+            if (distance < _toStarDis)
+            {
+                _toStarDis = distance;
+                _targetStar = target.transform.position;
+                _left = true;
+            }
+        }
+
+        foreach (Collider2D target in _starCollsB)
+        {
+            float distance = Vector3.Distance(target.transform.position, transform.position);
+            if (distance < _toStarDis)
+            {
+                _toStarDis = distance;
+                _targetStar = target.transform.position;
+                _left = false;
+            }
+        }
+        _ropeTrm.position = transform.position;
+    }
+
+    private bool IsRightTri()
+    {
+        float a = Vector3.Distance(_startVec, transform.position);
+        float b = Vector3.Distance(_targetStar, transform.position);
+        float c = Vector3.Distance(_targetStar, _startVec);
+        float p = Mathf.Pow(a, 2) + Mathf.Pow(b, 2) - Mathf.Pow(c, 2);
+        SetRope(b);
+        if (p > 0 && !_startOrbit)
+        {
+            _startOrbit = true;
+            if (_left) _orbitDir = 180 + 2.2f / b;
+            else _orbitDir = -2.2f / b;
+        }
+        return _startOrbit;
+    }
+
+    private void SetRope(float length)
+    {
+        _ropeTrm.right = (_targetStar - transform.position).normalized;
         _ropeTrm.localScale = new Vector3(length, 1);
         _ropeTrm.position = transform.position;
     }
@@ -111,11 +145,18 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.CompareTag("Wall") && !_startOrbit)
         {
+            Destroy(_ropeTrm.gameObject);
+            Destroy(gameObject);
+        }
+
+        if (collision.CompareTag("Star"))
+        {
+            Destroy(_ropeTrm.gameObject);
             Destroy(gameObject);
         }
     }
 
-    private void ScoreText()
+    private void SetScoreText()
     {
         _score = (int)((transform.position.y - _gameStartPos.y) / 2);
 
