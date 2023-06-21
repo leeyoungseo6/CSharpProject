8d204ca6c51209d7a09110ac50db6f2ccecf3bf5
gksdnfgksdnf <123638927+gksdnfgksdnf@users.noreply.github.com>
2023-06-21 15:45:50 +0900

    Update CreatStar.cs

--- Assets/01.Scripts/Hanul/CreatStar.cs
+++ Assets/01.Scripts/Hanul/CreatStar.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 public class CreatStar : MonoBehaviour
 {
-    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject _enemyPrefab;
     Transform _player;
+
+    int _prevscore = 0;
     void Start()
     {
         _player = FindObjectOfType<PlayerController>().transform;
@@ -13,17 +15,19 @@ public class CreatStar : MonoBehaviour
     private void Update()
     {
         transform.position = new Vector2(0, _player.position.y);
+
+
     }
     IEnumerator SpawnEnemy()
     {
         while (true)
         {
-            if(!Input.GetKey(KeyCode.Space))
+            if(GameManager.instance.Score > _prevscore + Random.Range(1, 2f))
             {
-
+                _prevscore = GameManager.instance.Score;
                 float x = Random.Range(0.7f, 1.4f);
                 yield return new WaitForSeconds(x);
-                GameObject enemy = Instantiate(enemyPrefab);
+                GameObject enemy = Instantiate(_enemyPrefab);
                 int rdIndex = Random.Range(0, transform.childCount);
                 enemy.transform.position = transform.GetChild(rdIndex).position;
             }
